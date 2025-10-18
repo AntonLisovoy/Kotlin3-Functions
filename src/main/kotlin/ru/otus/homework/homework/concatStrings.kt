e9a11977e726b6fed831b0ecbdb648e62b62e190
Антон Лисовой <alisovoy@moysklad.ru>
2025-10-18 11:59:34 +0300

    Функция с необязательным параметром и позиционными параметрами

--- src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
+++ src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
@@ -2,8 +2,9 @@ package ru.otus.homework.homework
 
 fun main() {
     check("str1 str2 str3" == concatStrings("str1", "str2", "str3"))
+    check("str1,str2,str3" == concatStrings("str1", "str2", "str3", separator = ','))
 }
 
-fun concatStrings(vararg strings: String): String {
-    TODO("Implement `concatStrings`")
+fun concatStrings(vararg strings: String, separator: Char = ' '): String {
+    return strings.joinToString(separator.toString())
 }
